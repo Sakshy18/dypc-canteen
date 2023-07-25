a57c4177eb966049979431f43388ee3d3488dc90
Sakshi Thombre <84434398+Sakshy18@users.noreply.github.com>
2023-07-25 14:02:19 +0530

    final commit from front-end side

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import Viewreport from "./Components/Viewreport";
+import Team from "./Components/Team";
+import PrintBill from "./Components/PrintBill";
+import "./Assets/Styles/Home.css";
+import "./Assets/Styles/Team.css";
+import "./Assets/Styles/PrintBill.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/viewreport" element={<Viewreport />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/printbill" element={<PrintBill />} />
+      </Routes>
   );
 }
-
-export default App;
---
+++ src/Data/Teamdata.js
@@ -0,0 +1,25 @@
+import Sakshi from "../Assets/Images/pic_profile.jpg";
+import Sweta from "../Assets/Images/sweta-profile.jpg";
+
+
+const Teamdata = [
+    {
+        name: "Sakshi Thombre",
+        role: "Developer",
+        github: "https://github.com/Sakshy18",
+        linkedin: "https://www.linkedin.com/in/sakshi-thombre-821600214",
+        mail: "mailto:isakshy18@gmail.com",
+        image: Sakshi
+    },
+    {
+        name: "Sweta Singh",
+        role: "Developer",
+        github: "https://github.com/72002sweta",
+        linkedin: "https://www.linkedin.com/in/sweta-singh-385a04221",
+        mail: "mailto:72002sweta@gmail.com",
+        image: Sweta
+    },
+    
+];
+
+export default Teamdata;
